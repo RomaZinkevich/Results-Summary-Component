0c434a29c6b2b30a9f5106fc30211facbca7578a
RomaZinkevich <zra1903@mail.ru>
2024-01-18 20:36:27 +0200

    added fetch from local .json file

---
+++ main.js
@@ -0,0 +1,25 @@
+
+window.onload = function() {
+    const testClass = document.getElementsByClassName('test');
+    const testResults = document.querySelectorAll('p.result')
+    const testIcons = document.getElementsByClassName('icon');
+    const testTitle = document.querySelectorAll('.test-title p');
+    const finalResult = document.querySelector('.result-container .result');
+    console.log(finalResult)
+    fetch("./data.json")
+    .then(result => result.json())
+    .then(data => {
+        let count = 0;
+        let sumScore = 0;
+        data.forEach(el => {
+            testClass[count].id = el.category.toLowerCase();
+            testResults[count].innerHTML = el.score;
+            testIcons[count].src = el.icon;
+            testTitle[count].innerHTML = el.category;
+            sumScore += el.score;
+            count++;
+        });
+        finalResult.innerHTML = Math.floor(sumScore/4);
+    })
+
+};
